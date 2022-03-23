5fad694fdbe8d678c6df90a84df89ea641d757f0
Kale Halter <kale.halter@gmail.com>
2022-03-22 19:36:45 -0600

    feat: added simple message interval

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client } = require('discord.js');
 const { token } = require('./config.json');
 
 const start = async () => {
-  const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+  const client = new Client({
+    intents: ['GUILDS', 'GUILD_MESSAGES']
+  });
+
+  client.on('ready', () => {
+    console.log('Ready!');
+
+    const channel = client.channels.cache.find(ch => {
+      return ch.type == 'GUILD_TEXT' && ch.name == 'chip-check'
+    });
+
+    if (channel) {
+      // Run every minute
+      setInterval(() => {
+        const date = new Date();
+        if (date.getHours() == 14 && date.getMinutes() == 00) {
+          channel.send('Chip Check :Scoops: :Rounds:');
+        }
+      }, 60000)
+    }
+  });
 
   await client.login(token);
 }
