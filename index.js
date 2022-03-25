8bcb4467feb7d63a5e0e4b5c84502584811ca26a
Kale Halter <kale.halter@gmail.com>
2022-03-24 21:18:00 -0600

    feat: add emoji reactions

--- index.js
+++ index.js
@@ -3,22 +3,28 @@ const { token } = require('./config.json');
 
 const start = async () => {
   const client = new Client({
-    intents: ['GUILDS', 'GUILD_MESSAGES']
+    intents: [
+      'GUILDS',
+      'GUILD_MESSAGES',
+      'GUILD_MESSAGE_REACTIONS',
+    ]
   });
 
   client.on('ready', () => {
     console.log('Ready!');
 
     const channel = client.channels.cache.find(ch => {
-      return ch.type == 'GUILD_TEXT' && ch.name == 'chip-check'
+      return ch.type == 'GUILD_TEXT' && ch.name == 'chip-check';
     });
 
     if (channel) {
       // Run every minute
-      setInterval(() => {
+      setInterval(async () => {
         const date = new Date();
         if (date.getHours() == 14 && date.getMinutes() == 00) {
-          channel.send('Chip Check :Scoops: :Rounds:');
+          const message = await channel.send({ content: 'Chip Check', fetch: true });
+          message.react('<:Rounds:955271228833275944>');
+          message.react('<:Scoops:955271245706965032>');
         }
       }, 60000)
     }
